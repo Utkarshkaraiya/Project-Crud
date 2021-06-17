3152bfb4c96dc11b799466af466969f81a38c75e
Utkarshkaraiya <karaiyautkarsh@gmail.com>
2021-06-17 14:38:18 +0530

    Updates

---
+++ precache-manifest.5969f966005ee0fa35e4d97a8a795eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7901cca968c6348e01ee352c8030a8",
+    "url": "/Project-Crud/index.html"
+  },
+  {
+    "revision": "225d609266a502b38eb3",
+    "url": "/Project-Crud/static/css/main.5d6b07b6.chunk.css"
+  },
+  {
+    "revision": "6700fa1a468cd3e87f72",
+    "url": "/Project-Crud/static/js/2.21b6cbb1.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/Project-Crud/static/js/2.21b6cbb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225d609266a502b38eb3",
+    "url": "/Project-Crud/static/js/main.44470ec9.chunk.js"
+  },
+  {
+    "revision": "52e5dbbe397a7102f3dd",
+    "url": "/Project-Crud/static/js/runtime-main.01a94f3c.js"
+  }
+]);
